8ffb560131e58762112fb01e68d5498aa80c5404
belongtothenight <dachuan516@gmail.com>
2022-11-19 00:15:42 +0800

    fininshed list_file.sh

--- src/list_file.sh
+++ src/list_file.sh
@@ -1,21 +1,61 @@
 #!/bin/bash
 # Usage: export a list of file directory with specified filetype only
 # Author: belongtothenight
+# --------------------------------------------------------------------------
 
-search_dir="/run/media/cdc/Transcend/Note_Database/YouTube/YT Database/YTD File Video/YTDFV Output-DDMS/"
-export_dir="/home/cdc/Documents/bash_scripts/record/mp4_files.txt"
+# start program
 
-# single layer
-function single_layer(){
-	echo "executing single layer function"
-	for file in "$search_dir"/*; do
+# get user input
+echo "Input filetype to list: "
+read filetype
+echo "Input export filename (with .txt): "
+read filename
+echo "Mode: multi-layer/single-layer (m/s/[n]): "
+read mode
+
+# variable
+# search_dir="/run/media/cdc/Transcend/Note_Database/YouTube/YT Database/YTD File Video/YTDFV Output-DDMS/"
+# search_dir="/run/media/cdc/Transcend/Note_Database/YouTube/YT Database/YTD File Video/"
+search_dir="/run/media/cdc/Transcend/"
+export_dir="/home/cdc/Documents/bash_scripts/record/$filename"
+# --------------------------------------------------------------------------
+
+# function
+function single_layer_list(){
+	for file in "$1"/*; do
 		ext="${file##*.}"
 		# echo $ext
-		if [[ $ext == mp4 ]]; then
-			echo "$file"
-			echo "$file" >> $export_dir
+		if [[ $ext == $filetype ]]; then
+			printf '\r%s' "$file"
+			echo "$file" >> "$export_dir"
 		fi
 	done
 }
+	function multi_layer_list(){
+	for path in "$1"/*; do
+		if [ -d "$path" ]; then
+			printf '\r%s' "${path#$search_dir}"
+			multi_layer_list "$path"
+		elif [ -f "$path" ]; then
+			ext="${path##*.}"
+			if [[ $ext == $filetype ]]; then
+				# echo "$path"
+				echo "$path" >> "$export_dir"
+			fi
+		fi
+	done
+}
+# --------------------------------------------------------------------------
 
-single_layer
+# main
+if [[ $mode == s ]]; then
+	echo "executing single-layer listing"
+	single_layer_list "$search_dir"
+elif [[ $mode == m ]]; then
+	echo "executing multi-layer listing"
+	multi_layer_list "$search_dir"
+elif [[ $mode == n ]]; then
+	echo "exiting..."
+else
+	echo "invalid mode, exiting..."
+fi
