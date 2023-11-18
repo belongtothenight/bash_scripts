b840cf0d15314fa9a1fed3aa55c2011ea68f0202
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-11-18 15:05:21 +0800

    increase search depth

--- src/update_all_repo.sh
+++ src/update_all_repo.sh
@@ -2,4 +2,4 @@
 
 #Ref: https://stackoverflow.com/questions/7470165/how-to-go-to-each-directory-and-execute-a-command
 cd ~/Documents/GitHub
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && echo 'Pulling from source ...' && git pull" \;
+find . -maxdepth 2 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && echo 'Pulling from source ...' && git pull" \;
