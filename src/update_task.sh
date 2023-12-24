18c71231ec075c1c105ff7c15c25113beb0a8db6
belongtothenight <dachuan516@gmail.com>
2023-12-25 00:32:04 +0800

    add script to run update and output to log file

---
+++ src/update_task.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# This script will be run with systemd service
+# This process can be monitored with "${HOME}/Documents/list_dir.sh"
+# Ref: https://unix.stackexchange.com/questions/26728/prepending-a-timestamp-to-each-line-of-output-from-a-command
+log_file="${HOME}/Documents/update_log.txt"
+log_format="%Y/%m/%d-%H:%M:%S"
+msg_preappend=">>>"
+
+# pipe message
+function pmsg () {
+	echo -e "[$(date +${log_format})] ${msg_preappend} $1" &>> $log_file
+}
+
+# execute program and pipe output
+function epapo () {
+	# Method 1: don't use with commmand that taks a long time to execute
+	#$1 | awk -v log_format=${log_format} '{print strftime("["log_format"]"), $0}' &>> $log_file
+	# Method 2: sudo apt-get install moreutils
+	$1 2>&1 | ts "[$log_format]" 2>&1 &>> $log_file
+}
+
+# execute shell and pipe output
+function esapo () {
+	$1 2>&1 | ts "[$log_format]" | tee -a $log_file
+}
+
+echo -e "\n\r" &>> $log_file
+pmsg "update_task.sh started!"
+
+# Task 1 => Update System
+pmsg "Updating system"
+epapo "sudo apt-get update"
+pmsg "Upgrading system"
+epapo "sudo apt-get upgrade -y"
+
+# Task 2 => Clone all repos
+pmsg "Cloning all repos with clone_all_repo.sh"
+#${HOME}/Documents/clone_all_repo.sh &>> $log_file
+esapo "${HOME}/Documents/clone_all_repo.sh"
+
+# Task 3 => Update all repos
+pmsg "Updating all repos with update_all_repo.sh"
+esapo "${HOME}/Documents/update_all_repo.sh"
+
+pmsg "update_task.sh ended!"
