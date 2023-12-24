70033d9f8ec0aa2f1b5ca974df7d25a1dc31ae8f
belongtothenight <dachuan516@gmail.com>
2023-12-25 02:14:24 +0800

    change daily execution to start up since no RTC available

--- src/setup_update_task.sh
+++ src/setup_update_task.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# This script set update_task.sh to run as service with systemd
-# OnBootSec is set quit large to make sure apt isn't in-use
-# The auto-restart function isn't working properly likely due to Raspberry Pi 4 isn't connected with battery powered RTC
+# This script set update_task.sh to run as service with systemd, whenever system boot up
+# The timer isn't working properly likely due to Raspberry Pi 4 isn't connected with battery powered RTC
 # Ref: https://unix.stackexchange.com/questions/48203/run-script-once-a-day-with-systemd
 # Ref: https://www.freedesktop.org/software/systemd/man/latest/systemd.timer.html
 script_path="${HOME}/Documents/update_task.sh"
 daemon_name="update_task.service"
 user_name="user"
+sleep_time_min=3
 systemd_dir="/lib/systemd/system/${daemon_name}"
 
 # Add service to systemd
 sudo bash -c "echo -e \"\
 [Unit]\n\
-Description=Daily automatic update script with update_task.sh\n\
+Description=automatic startup update script with update_task.sh\n\
 After=multi-user.target\n\
 StartLimitIntervalSec=0\n\
 \n\
@@ -20,11 +20,9 @@ StartLimitIntervalSec=0\n\
 User=$user_name\n\
 Type=simple\n\
 KillMode=mixed\n\
+TimeoutSec=$((60 * (sleep_time_min + 1)))
+ExecStartPre=/bin/sleep $((60 * sleep_time_min))
 ExecStart=${script_path}\n\
-\n\
-[Timer]\n\
-OnBootSec=5min\n\
-OnUnitActiveSec=1d\n\
 \n\
 [Install]\n\
 WantedBy=multi-user.target\n\
@@ -44,5 +42,5 @@ sudo systemctl enable $daemon_name
 echo "Enabled daemon $daemon_name"
 
 # Start Service
-sudo systemctl start $daemon_name
+sudo systemctl start $daemon_name &
 echo "Started daemon $daemon_name"
