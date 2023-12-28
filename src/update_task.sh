03ff334f35488dab42964cc094e20e7ccff503fd
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-29 02:35:41 +0800

    Adjust tasks in script to be more easily adjustable by settings at the start

--- src/update_task.sh
+++ src/update_task.sh
@@ -3,15 +3,33 @@
 # This process can be monitored with "${HOME}/Documents/list_dir.sh"
 # Ref: https://unix.stackexchange.com/questions/26728/prepending-a-timestamp-to-each-line-of-output-from-a-command
 # Req: moreutils
-log_file="${HOME}/Documents/update_task.log"
+
+# Settings
+base_dir="${HOME}/Documents"
+repo_dir="${base_dir}/GitHub"
+log_file="${base_dir}/update_task.log"
 log_format="%Y/%m/%d-%H:%M:%S"
 msg_preappend=">>>"
 pkg_manager="nala" # default: apt-get
-pw_file="${HOME}/Documents/pw.txt"
+
+# Tasks (1=Active)
+t0=1
+t1=0
+t2=1
+t3=1
+t4=1
+t5=1
+
+active=1
 
 # pipe message
 function pmsg () {
 	echo -e "[$(date +${log_format})] ${msg_preappend} $1" &>> $log_file
+}
+
+# pipe message without preappending
+function pmsgwp () {
+	echo -e "[$(date +${log_format})] $1" &>> $log_file
 }
 
 # execute program and pipe output
@@ -30,18 +48,84 @@ function esapo () {
 echo -e "\r" &>> $log_file
 pmsg "update_task.sh started!"
 
+# Task 0 => Parameters
+if [ $t0 -eq $active ]; then
+	pmsg "Task 0: List out parameters!"
+	pmsgwp "Settings ==="
+	pmsgwp "base_dir:       $base_dir"
+	pmsgwp "repo_dir:       $repo_dir"
+	pmsgwp "log_file:       $log_file"
+	pmsgwp "log_format:     $log_format"
+	pmsgwp "msg_preappend:  $msg_preappend"
+	pmsgwp "pkg_manager:    $pkg_manager"
+	pmsgwp "Tasks ======"
+	if [ $t0 -eq $active ]; then
+		pmsgwp "Task 0: Activated"
+	else
+		pmsgwp "Task 0: Deactivated"
+	fi
+	if [ $t1 -eq $active ]; then
+		pmsgwp "Task 1: Activated"
+	else
+		pmsgwp "Task 1: Deactivated"
+	fi
+	if [ $t2 -eq $active ]; then
+		pmsgwp "Task 2: Activated"
+	else
+		pmsgwp "Task 2: Deactivated"
+	fi
+	if [ $t3 -eq $active ]; then
+		pmsgwp "Task 3: Activated"
+	else
+		pmsgwp "Task 3: Deactivated"
+	fi
+	if [ $t4 -eq $active ]; then
+		pmsgwp "Task 4: Activated"
+	else
+		pmsgwp "Task 4: Deactivated"
+	fi
+	if [ $t5 -eq $active ]; then
+		pmsgwp "Task 5: Activated"
+	else
+		pmsgwp "Task 5: Deactivated"
+	fi
+fi
+
 # Task 1 => Update System
-#pmsg "Updating system"
-#epapo "${pkg_manager} update"
-#pmsg "Upgrading system"
-#epapo "${pkg_manager} upgrade -y"
+if [ $t1 -eq $active ]; then
+	pmsg "Task 1: Update system"
+	pmsg "Updating system"
+	epapo "${pkg_manager} update"
+	pmsg "Upgrading system"
+	epapo "${pkg_manager} upgrade -y"
+fi
 
 # Task 2 => Clone all repos
-pmsg "Cloning all repos with clone_all_repo.sh"
-esapo "${HOME}/Documents/GitHub/clone_all_repo.sh"
+if [ $t2 -eq $active ]; then
+	pmsg "Task 2: Clone all repos"
+	pmsg "Cloning all repos with clone_all_repo.sh"
+	esapo "${repo_dir}/clone_all_repo.sh"
+fi
 
 # Task 3 => Update all repos
-pmsg "Updating all repos with update_all_repo.sh"
-esapo "${HOME}/Documents/GitHub/update_all_repo.sh"
+if [ $t3 -eq $active ]; then
+	pmsg "Task 3: Update all repos"
+	pmsg "Updating all repos with update_all_repo.sh"
+	esapo "${repo_dir}/update_all_repo.sh"
+fi
+
+# Task 4 => Print out repo tree
+if [ $t4 -eq $active ]; then
+	pmsg "Task 4: Print out repo tree"
+	pmsg "Printing local repo tree at ${repo_dir}"
+	epapo "tree ${repo_dir} -L 2"
+fi
+
+# Task 5 => Get total repo size
+if [ $t5 -eq $active ]; then
+	pmsg "Task 5: Get total repo size"
+	pmsg "Getting total repo size locally at ${repo_dir}"
+	epapo "du ${repo_dir} -h -s"
+fi
 
 pmsg "update_task.sh ended!"
