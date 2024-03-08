ede9fb9bbd258a307c29f067b1b404e8865c579b
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-03-08 17:06:56 +0800

    add proc_ss.sh
    Add script for automatic renaming and moving entire directory of file of certain filetype (file extension)

---
+++ src/proc_ss.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# PIPELINE
+# 1. Move pictures from source to destination
+# 2. Rename them automatically remove spaces in filename
+
+#SRC="/home/$(whoami)/Pictures"
+SRC="/run/media/11278041/44AB-5704/ICL/w3/hw3-1"
+DST="/run/media/11278041/44AB-5704/ICL/w3/hw3-1"
+RPC="_"
+TYP="png"
+
+# Check $SRC
+if [ -d "$SRC" ]; then
+    :
+else
+    echo ">> SRC path doesn't exist"
+    exit
+fi
+
+# Check $DST
+if [ -d "$DST" ]; then
+    :
+else
+    echo ">> DST path doesn't exist"
+    exit
+fi
+
+cd $SRC
+fcnt=0
+
+# Count files
+for f in *.$TYP; do
+    ((fcnt++))
+done
+
+echo ">> Processing files:"
+fcnt_t=0
+for f in *.$TYP; do
+    ((fcnt_t++))
+    fsrc="$SRC/$f"
+    fdst="$DST/${fcnt_t}_${f// /$RPC}" # Replace space with specified character
+    echo "($fcnt_t/$fcnt) $fsrc -> $fdst"
+    mv "$fsrc" "$fdst"
+done
+
+echo ">> Process finished!"
