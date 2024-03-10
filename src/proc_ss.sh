5edea099e6d553928477e2f7624ff9960841a117
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-03-10 16:49:41 +0800

    Add new section to display changes and convert all files with dos2unix

--- src/proc_ss.sh
+++ src/proc_ss.sh
@@ -34,6 +34,25 @@ for f in *.$TYP; do
     ((fcnt++))
 done
 
+# Display following changes and ask for permission
+echo ">> Following changes:"
+fcnt_t=0
+for f in *.$TYP; do
+    ((fcnt_t++))
+    fsrc="$SRC/$f"
+    fdst="$DST/${fcnt_t}_${f// /$RPC}" # Replace space with specified character
+    echo "($fcnt_t/$fcnt) $fsrc -> $fdst"
+done
+read -p ">> Are you sure to continue? [Yy] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    :
+else
+    echo ">> Process terminated!"
+    exit
+fi
+
+# Process files
 echo ">> Processing files:"
 fcnt_t=0
 for f in *.$TYP; do
