7a08440fdd35c67f14f6500e2f066907cc8cb959
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-03-11 11:47:24 +0800

    Add new var for filename keeping switch, and var checker

--- src/proc_ss.sh
+++ src/proc_ss.sh
@@ -4,17 +4,26 @@
 # 1. Move pictures from source to destination
 # 2. Rename them automatically remove spaces in filename
 
+# VARIABLES
+
+# -- source path, do not include the last slash at the end
 #SRC="/home/$(whoami)/Pictures"
-SRC="/run/media/11278041/44AB-5704/ICL/w3/hw3-1"
-DST="/run/media/11278041/44AB-5704/ICL/w3/hw3-1"
+SRC="/home/$(whoami)"
+# -- destination path, do not include the last slash at the end
+#DST="/run/media/11278041/44AB-5704/ICL/w3/hw3-1"
+DST="/home/$(whoami)"
+# -- filtering filetype, the "dot" is required
+TYP=".png"
+# -- Keep Original Name
+KON=false
+# -- Replace original space with this character, only active if $KON is true
 RPC="_"
-TYP="png"
 
 # Check $SRC
 if [ -d "$SRC" ]; then
     :
 else
-    echo ">> SRC path doesn't exist"
+    echo ">> \$SRC path doesn't exist"
     exit
 fi
 
@@ -22,7 +31,23 @@ fi
 if [ -d "$DST" ]; then
     :
 else
-    echo ">> DST path doesn't exist"
+    echo ">> \$DST path doesn't exist"
+    exit
+fi
+
+# Check $TYP
+if [[ $TYP == .* ]]; then
+    :
+else
+    echo ">> \$TYP format isn't correct, should start with a \"dot\""
+    exit
+fi
+
+# Check $KON
+if [ $KON = true ] || [ $KON = false ]; then
+    :
+else
+    echo ">> \$KON format isn't correct, should be either \"true\" or \"false\""
     exit
 fi
 
@@ -30,17 +55,21 @@ cd $SRC
 fcnt=0
 
 # Count files
-for f in *.$TYP; do
+for f in *$TYP; do
     ((fcnt++))
 done
 
 # Display following changes and ask for permission
 echo ">> Following changes:"
 fcnt_t=0
-for f in *.$TYP; do
+for f in *$TYP; do
     ((fcnt_t++))
     fsrc="$SRC/$f"
-    fdst="$DST/${fcnt_t}_${f// /$RPC}" # Replace space with specified character
+    if [ "$KON" = true ]; then
+        fdst="$DST/${fcnt_t}_${f// /$RPC}" # Replace space with specified character
+    else
+        fdst="$DST/${fcnt_t}$TYP"
+    fi
     echo "($fcnt_t/$fcnt) $fsrc -> $fdst"
 done
 read -p ">> Are you sure to continue? [Yy] " -n 1 -r
@@ -55,10 +84,14 @@ fi
 # Process files
 echo ">> Processing files:"
 fcnt_t=0
-for f in *.$TYP; do
+for f in *$TYP; do
     ((fcnt_t++))
     fsrc="$SRC/$f"
-    fdst="$DST/${fcnt_t}_${f// /$RPC}" # Replace space with specified character
+    if [ "$KON" = true ]; then
+        fdst="$DST/${fcnt_t}_${f// /$RPC}" # Replace space with specified character
+    else
+        fdst="$DST/${fcnt_t}$TYP"
+    fi
     echo "($fcnt_t/$fcnt) $fsrc -> $fdst"
     mv "$fsrc" "$fdst"
 done
